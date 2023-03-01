419405bef681c2f391be5d73e478be85bebf3a02
krishnadev7 <krishnadevv12@gmail.com>
2023-03-01 22:33:28 +0530

    navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { Box } from '@mui/material'
+import {  Route, Routes } from 'react-router-dom'
 import './App.css'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import ExerciseDetail from './pages/ExerciseDetail'
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div >
-      hello
-    </div>
+    <Box width='400px' sx={{width: {xl: '1488px'}}} m='auto'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/exercise/:id' element={<ExerciseDetail/>}/>
+      </Routes>
+      <Footer/>
+    </Box>
   )
 }
 
