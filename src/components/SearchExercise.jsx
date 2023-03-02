38fdcddc05579fb47dd557d2bcb48074c65f36f6
krishnadev7 <krishnadevv12@gmail.com>
2023-03-02 22:41:58 +0530

    rapid api implemented

--- src/components/SearchExercise.jsx
+++ src/components/SearchExercise.jsx
@@ -1,8 +1,44 @@
 import { Button, TextField, Typography } from '@mui/material';
 import { Box, Stack } from '@mui/system';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { exerciseOptions, fetchData } from '../utlis/fetchData';
 
 const SearchExercise = () => {
+  const [search, setSearch] = useState('');
+  const [exercises, setExercises] = useState([]);
+  const [bodyParts,setBodyParts] = useState([]);
+
+  useEffect(() => {
+    const fetchExerciseData = async() => {
+      const bodyPartsData = await fetchData(
+        'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
+        exerciseOptions
+      );
+        setBodyParts(['all', ...bodyPartsData]);
+    }
+    fetchExerciseData();
+  },[])
+
+  const handleChange = async e => {
+    e.preventDefault();
+    if (search) {
+      const exerciseData = await fetchData(
+        'https://exercisedb.p.rapidapi.com/exercises',
+        exerciseOptions
+      );
+
+      const searchedExercises = exerciseData.filter(
+        exercise =>
+          exercise.name.toLowerCase().includes(search) ||
+          exercise.target.toLowerCase().includes(search) ||
+          exercise.equipement.toLowerCase().includes(search) ||
+          exercise.bodyPart.toLowerCase().includes(search)
+      );
+      setSearch('');
+      setExercises(searchedExercises);
+    }
+  };
+
   return (
     <Stack alignItems='center' justifyContent='center' mt='37px' p='20px'>
       <Typography
@@ -28,14 +64,28 @@ const SearchExercise = () => {
             borderRadius: '40px',
           }}
           height='76px'
-          value=''
-          onChange={e => {}}
+          value={search}
+          onChange={e => {
+            setSearch(e.target.value.toLowerCase());
+          }}
           placeholder='Search Exercises'
           type='text'
         />
-        <Button className="search-btn"
-         sx={{bgcolor:'#FF2625',color:'#fff', textTransform:'none',width:{lg:'175px', xs:'80px'},fontSize:{lg:'20px',xs:'14px'},height:'56px',postion:'absolute'}}>
-            Search
+        <Button
+          className='search-btn'
+          onClick={handleChange}
+          sx={{
+            bgcolor: '#FF2625',
+            color: '#fff',
+            textTransform: 'none',
+            width: { lg: '175px', xs: '40px' },
+            fontSize: { lg: '20px', xs: '14px' },
+            height: '56px',
+            postion: 'absolute',
+            right: '0',
+          }}
+        >
+          Search
         </Button>
       </Box>
     </Stack>
