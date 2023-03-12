ed04af408cd572b6ff926987863eb1705c87520f
krishnadev7 <krishnadevv12@gmail.com>
2023-03-13 00:15:46 +0530

    exercise slice bug

--- src/components/Exercise.jsx
+++ src/components/Exercise.jsx
@@ -1,10 +1,35 @@
 import { Box, Typography, Stack, Pagination } from '@mui/material';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { exerciseOptions, fetchData } from '../utlis/fetchData';
 import ExerciseCard from './ExerciseCard';
 
 const Exercise = ({ exercises, bodyPart, setExercises }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const exercisesPerPage = 9;
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      let exercisesData = [];
+
+      if (bodyPart === 'all') {
+        exercisesData = await fetchData(
+          'https://exercisedb.p.rapidapi.com/exercises',
+          exerciseOptions
+        );
+      } else {
+        exercisesData = await fetchData(
+          `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`,
+          exerciseOptions
+        );
+      }
+
+      setExercises(exercisesData);
+    };
+
+    fetchExercisesData();
+  }, [bodyPart]);
+
+  // pagination mui
 
   const indexOfLastExercise = currentPage * exercisesPerPage;
   const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
@@ -14,6 +39,7 @@ const Exercise = ({ exercises, bodyPart, setExercises }) => {
     setCurrentPage(value);
     window.scrollTo({ top: '1800', behavior: 'smooth' });
   };
+  
   return (
     <Box id='exercises' sx={{ mt: { lg: '109px' } }} mt='50px' p='20px'>
       <Typography variant='h3'>Showing Results...</Typography>
